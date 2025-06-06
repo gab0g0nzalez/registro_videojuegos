ab04a85480fc0bd21be34fd341ebc502ec05a866
gab0g0nzalez <gf.gonzalez@duocuc.cl>
2025-06-06 11:41:33 -0400

    Desafío: Validaciones agregadas con try/except

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,19 +8,48 @@ while True:
     print('4.   Eliminar videojuego')
     print('5.   Salir')
 
-    opcion=input('Seleccione una opción (1-5):  ')
+    opcion=input('Seleccione una opción (1-5) :  ')
 
     if opcion=='1':
-        codigo=int(input('Ingrese el código del videojuego: '))
-        nombre=input('Ingrese el nombre del videojuego: ')
-        genero=input('Ingrese el género del videojuego: ')
+        while True:
+            try:
+                codigo=int(input('Ingrese el código del videojuego: '))
+                if codigo<=0:
+                    print('Error!   Debe ingresar números mayores a 0')
+                else:
+                    break
+            except ValueError:
+                print('Error!   Debe ingresar números enteros.')
+        while True:
+            nombre=input('Ingrese el nombre del videojuego: ')
+            if nombre.isspace() or nombre.isdigit():
+                print('Error!   Debe ingresar letras o números.')
+            else:
+                break
+
+        while True:
+            genero=input('Ingrese el género del videojuego: ')
+            if genero.isalpha():
+                break
+            else:
+                print('Error!   Debe ingresar letras.')
 
         print('\nPlataformas disponibles:')
         print('1.   PC')
         print('2.   PS5')
         print('3.   Xbox Series X')
         print('4.   Nintendo Switch')
-        plataforma_codigo=int(input('Seleccione el número de la plataforma: '))
+
+        while True:
+            try:
+                plataforma_codigo=int(input('Seleccione el número de la plataforma: '))
+                if plataforma_codigo in range(1,5):
+                    break
+                else:
+                    print('Error!   Debe ingresar números entre 1-4.')
+            except ValueError:
+                print('Error!   Debe ingresar números enteros.')
+
         plataforma=plataformas[plataforma_codigo-1]
         videojuego={
             'codigo':codigo,
@@ -36,21 +65,54 @@ while True:
         else:
             print('\n--- LISTA DE VIDEOJUEGOS ---')
             for v in videojuegos:
-                print(f'Código: {v[codigo]}, Nombre: {v[nombre]}, Género: {v[genero]}, Plataforma: {v[plataforma]}')
+                print(f'Código: {v['codigo']}, Nombre: {v['nombre']}, Género: {v['genero']}, Plataforma: {v['plataforma']}')
     elif opcion=='3':
-        codigo= int(input('Ingrese el código del videojuego a modificar:    '))
+        while True:
+            try:
+                codigo= int(input('Ingrese el código del videojuego a modificar:    '))
+                if codigo<=0:
+                    print('Error!   Debe ingresar números mayores a 0')
+                else:
+                    break
+            except ValueError:
+                print('Error!   Debe ingresar números enteros.')
+
         encontrado=False
         for v in videojuegos:
             if v['codigo']==codigo:
-                v['nombre']=input('Nuevo nombre:   ')
-                v['genero']=input('Nuevo género:    ')
+
+                while True:
+                    nombre=input('Nuevo nombre:   ')
+                    if nombre.isspace() or nombre.isdigit():
+                        print('Error!   Debe ingresar letras o números.')
+                    else:
+                        break
+
+                v['nombre']=nombre
+
+                while True:
+                    genero=input('Nuevo género:    ')
+                    if genero.isalpha():
+                        break
+                    else:
+                        print('Error!   Debe ingresar letras.')
+
+                v['genero']=genero
                 print('\nPlataformas disponibles:')
                 print('1.   PC')
                 print('2.   PS5')
                 print('3.   Xbox Series X')
                 print('4.   Nintendo Switch')
 
-                plataforma_codigo=int(input('Seleccione el número de la plataforma: '))
+                while True:
+                    try:
+                        plataforma_codigo=int(input('Seleccione el número de la nueva plataforma: '))
+                        if plataforma_codigo in range(1,5):
+                            break
+                        else:
+                            print('Error!   Debe ingresar números entre 1-4.')
+                    except ValueError:
+                        print('Error!   Debe ingresar números enteros.')
 
                 print('Videojuego modificado correctamente.')
                 encontrado=True
@@ -58,7 +120,16 @@ while True:
         if not encontrado:
             print('Videojuego no encontrado.')
     elif opcion=='4':
-        codigo=int(input('Ingrese el código del videojuego a eliminar:  '))
+        while True:
+            try:
+                codigo=int(input('Ingrese el código del videojuego a eliminar:  '))#
+                if codigo<=0:
+                    print('Error!   Debe ingresar números mayores a 0')
+                else:
+                    break
+            except ValueError:
+                print('Error!   Debe ingresar números enteros.')
+
         eliminado=False
         for v in videojuegos:
             if v['codigo']==codigo:
